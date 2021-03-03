8010d83b4f5e84c8214f5cc60c7b229051ae197a
moro0823 <basell.4rm.0823@outlook.com>
2021-03-03 09:40:58 +0000

    名前でログインできるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    # ログイン時nameでできるようにする時に追加
   end
 end
