21d76c952ce7f512634a9dd2dc124c5e9bf70fdd
moro0823 <basell.4rm.0823@outlook.com>
2021-03-03 14:06:41 +0000

    基本機能搭載　next　ユーザー詳細画面　

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_052059) do
+ActiveRecord::Schema.define(version: 2021_03_03_103057) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
+    t.string "user"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
