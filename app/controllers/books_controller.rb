0bd0f14fc62b8caf4bf7b6ff25f170f2d628a348
moro0823 <basell.4rm.0823@outlook.com>
2021-03-07 09:56:45 +0000

    サクセスメッセージ NEXT エラ〜メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,7 +37,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
+     flash[:notice] = "Book was successfully update"
     else
      render:edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
-  
-  
+
+
   def new
   end
-  
-  
+
+
   def show
     @user = User.find(params[:id])
     @books = Book.all
     @books = @user.books
     @book = Book.new
-    @book.user_id = current_user.id 
+    @book.user_id = current_user.id
   end
-  
+
   def index
    @users = User.all
    @user = current_user
    @book = Book.new
-   @book.user_id = current_user.id 
+   @book.user_id = current_user.id
   end
-  
-  
+
+
   def edit
    @user = User.find(params[:id])
   end
-  
-  
+
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -34,18 +34,18 @@ class UsersController < ApplicationController
      render:edit
     end
   end
-  
-  
-  
-  
+
+
+
+
   private
    def book_params
     params.require(:book).permit(:title, :body)
     book_paramsの中に新規投稿フォームで入力されたデータが格納される
    end
-   
+
     def user_params
      params.require(:user).permit(:name, :profile_image,:introduction)
     end
-   
+
 end
